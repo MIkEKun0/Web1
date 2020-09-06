f82e255c721c7ea6ac2d4f4b2fcbd0b6bda85d77
MIkEKun0 <58865357+MIkEKun0@users.noreply.github.com>
2020-09-06 19:06:01 +0300

    Add files via upload

--- weblab1/server.php
+++ weblab1/server.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+//$_SESSION['rows'] = array();
 $start = microtime(true);
 $x = $_POST["Xbtn"];
 $x = (float) preg_replace("/,/", ".", $x);
@@ -19,17 +20,20 @@ function checkCoordinates($x, $y, $r){
     }
     return false;
 }
-$html = file_get_contents('index.html');
-$end = microtime(true) - $start;
+$answ = '';
 if(checkCoordinates($x,$y,$r)){
+    $answ = 'true';
+}else{
+    $answ = 'false';
+}
+$end = microtime(true) - $start;
+array_push($_SESSION['rows'],"<td>$x</td><td>$y</td><td>$r</td><td>$time</td><td>$end</td><td>$answ</td>");
+$html = file_get_contents('index.html');
      echo $html ;
      echo "<table id='out' align='center' border='1'>";
-     echo "<tbody><tr><td>$x</td>";
-     echo "<td>$y</td>";
-     echo "<td>$r</td>
-<td>$time</td><td>$end</td></tr></tbody>";
-     echo "</table>";
-}else{
-    echo "rot ebal";
-}
-
+     echo "<thead><tr><td>X</td><td>Y</td><td>R</td><td>Time</td><td>Current time</td><td>Result</td></tr></thead>";
+     echo "<tbody>";
+     foreach ($_SESSION["rows"] as $row){
+         echo "<tr>$row</tr>";
+     }
+     echo "</tbody></table>";
